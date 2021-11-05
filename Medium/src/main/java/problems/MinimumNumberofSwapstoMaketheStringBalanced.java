347af69e8d3bd8c645c3995d6c9ce074dbdc2dd2
hmaziad <hmaziad@murex.com>
2021-11-05 16:13:52 +0200

    optimitzed solution from leetcode

--- Medium/src/main/java/problems/MinimumNumberofSwapstoMaketheStringBalanced.java
+++ Medium/src/main/java/problems/MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -1,7 +1,23 @@
 package problems;
 
+//https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
 public class MinimumNumberofSwapstoMaketheStringBalanced {
     public static void main(String[] args) {
 
+        System.out.println(minSwaps("][]["));
+    }
+
+    public static int minSwaps(String s) {
+        int mismatches = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '[')
+                mismatches++;
+            else {
+                if (mismatches > 0)
+                    mismatches--;
+            }
+        }
+        return (mismatches + 1) / 2;
     }
 }
