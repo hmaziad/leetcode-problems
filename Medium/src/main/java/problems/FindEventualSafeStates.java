126d92a66e863b4aa6b210c38bb3982aed284b7b
hmaziad <hmaziad@murex.com>
2021-11-09 21:01:37 +0200

    2nd way

--- Medium/src/main/java/problems/FindEventualSafeStates.java
+++ Medium/src/main/java/problems/FindEventualSafeStates.java
@@ -3,15 +3,21 @@ package problems;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import static problems.FindEventualSafeStates.State.SAFE;
+import static problems.FindEventualSafeStates.State.UNSAFE;
+
 // https://leetcode.com/problems/find-eventual-safe-states/
+
+//TODO see color solution
 public class FindEventualSafeStates {
     public static void main(String[] args) {
 //        int[][] nodes = {{}, {0, 2, 3, 4}, {3}, {4}, {}};
         int[][] nodes = {{1, 2}, {2, 3}, {5}, {0}, {5}, {}, {}};
-        System.out.println(eventualSafeNodes(nodes));
+        System.out.println(eventualSafeNodes2(nodes));
     }
 
     public static List<Integer> eventualSafeNodes(int[][] graph) {
@@ -39,6 +45,40 @@ public class FindEventualSafeStates {
             }
         }
         safeNodes.add(i);
+        return true;
+    }
+
+    enum State {
+        SAFE,
+        UNSAFE
+    }
+
+    public static List<Integer> eventualSafeNodes2(int[][] graph) {
+
+        State[] states = new State[graph.length];
+        List<Integer> safeNodes = new ArrayList<>();
+
+        for (int node = 0; node < graph.length; node++) {
+            if (isSafe(graph, node, states)) {
+                safeNodes.add(node);
+            }
+        }
+        return safeNodes;
+    }
+
+    private static boolean isSafe(int[][] graph, int node, State[] states) {
+        if (states[node] != null) {
+            return states[node] == SAFE;
+        }
+
+        states[node] = UNSAFE;
+
+        for (int child : graph[node]) {
+            if (!isSafe(graph, child, states)) {
+                return false;
+            }
+        }
+        states[node] = SAFE;
         return true;
     }
 
