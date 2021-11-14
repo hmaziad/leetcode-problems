89ef7ff6281759da76b87613650d4ad05d3e1dc5
hmaziad <hmaziad@murex.com>
2021-11-14 12:32:06 +0200

    solved

--- Medium/src/main/java/problems/NextPermutationOwn.java
+++ Medium/src/main/java/problems/NextPermutationOwn.java
@@ -1,16 +1,81 @@
 package problems;
 
+import java.util.Arrays;
+
 public class NextPermutationOwn {
 
     public static void main(String[] args) {
-        int[] orig = {5, 4, 8, 9, 3, 5, 5, 1, 4, 2};
-        int[] res = {5, 4, 8, 9, 3, 5, 5, 4, 2, 1};
 
-        System.out.println(getMinAdjSwaps(orig, res));
+        System.out.println(getMinSwaps("5489355142",4));
+    }
+
+    public static int getMinSwaps(String num, int k) {
+
+        char[] nums = num.toCharArray();
+
+        while (k > 0) {
+            nextPermutation(nums);
+            k--;
+        }
+
+        return getMinAdjSwaps(num.toCharArray(), nums);
+
 
     }
 
-    private static int getMinAdjSwaps(int[] orig, int[] res) {
+    private static void nextPermutation(char[] arr) {
+
+        /**
+         * Algo:
+         *
+         * 1) find longest non-increasing suffix and get the pivot
+         * 2) swap it with the rightmost next greatest than pivot
+         * 3) reverse suffix
+         */
+
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        //1.
+
+        int last = arr.length - 2;
+
+        while (last >= 0) {
+            if (arr[last] < arr[last + 1]) {
+                break;
+            }
+            last--;
+        }
+
+        if (last == -1) {
+            return;
+        }
+
+        //2.
+
+        for (int i = arr.length - 1; i > last; i--) {
+            if (arr[i] > arr[last]) {
+                swap(arr, i, last);
+                break;
+            }
+        }
+
+        reverse(arr, last + 1);
+
+    }
+
+    private static void reverse(char[] arr, int start) {
+
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+
+    }
+
+
+    private static int getMinAdjSwaps(char[] orig, char[] res) {
 
         int swaps = 0;
 
@@ -26,7 +91,7 @@ public class NextPermutationOwn {
             }
 
             while (orig[i] != res[i]) {
-                swap(res,j - 1, j-- );
+                swap(res, j - 1, j--);
                 swaps++;
             }
         }
@@ -34,10 +99,10 @@ public class NextPermutationOwn {
         return swaps;
     }
 
-    private static void swap(int[] res, int a, int b) {
+    private static void swap(char[] arr, int a, int b) {
 
-        int temp = res[a];
-        res[a] = res[b];
-        res[b] = temp;
+        char temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
